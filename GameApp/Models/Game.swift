9ef5ebd40c0888cd33659bf33031bce0ccee27fa
Mitchman215 <mitchell_salomon@brown.edu>
2022-01-07 00:30:28 -0500

    Added more variables for each AvailableGames cases. Also added image assets for each game

--- GameApp/Models/Game.swift
+++ GameApp/Models/Game.swift
@@ -6,14 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum AvailableGames: String, CaseIterable {
     case ticTacToe = "Tic-Tac-Toe"
     case connect4 = "Connect Four"
     case chess = "Chess"
     
-    func getName() -> String {
-        self.rawValue
+    var name: String {self.rawValue}
+    var imageName: String {name}
+    var description: String {
+        switch self {
+        case .ticTacToe:
+            return """
+Tic-Tac-Toe is a 2 player game where each player takes turns placing either X's or O's on a 3x3 grid.
+The first player to get 3 of their marks in a row diagonally, horizontally, or vertically wins.
+If the entire grid is filled, the game is a draw.
+"""
+        case .connect4:
+            return """
+Connect Four is a 2 player game where each player takes turn dropping colored discs into a 6x7 suspended grid.
+The first player to get 5 of their discs in a row diagonally, horizontally, or vertically wins.
+If the entire grid is filled, the game is a draw.
+"""
+        case .chess:
+            return """
+Chess is a 2 player strategy game played on a 8x8 grid. The players take turns moving their pieces (either
+white or black). Each of the different types of pieces have their own rules for moving and can capture
+their opponents pieces.
+In order to win, one player must checkmate the other by threatening to capture their king
+and preventing the king from being able to escape.
+"""
+        }
     }
 }
 
