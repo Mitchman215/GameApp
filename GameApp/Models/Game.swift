c198f1f58d4757d980fb3e14121c5dcac9cd5c00
Mitchman215 <mitchell_salomon@brown.edu>
2022-01-09 23:45:24 -0500

    Created rudimentary objects for players, pieces, and game protocol. Indented descriptions

--- GameApp/Models/Game.swift
+++ GameApp/Models/Game.swift
@@ -19,32 +19,63 @@ enum AvailableGames: String, CaseIterable {
         switch self {
         case .ticTacToe:
             return """
-Tic-Tac-Toe is a 2 player game where each player takes turns placing either X's or O's on a 3x3 grid.
-The first player to get 3 of their marks in a row diagonally, horizontally, or vertically wins.
-If the entire grid is filled, the game is a draw.
-"""
+                    Tic-Tac-Toe is a 2 player game where each player takes turns placing either X's or O's on a 3x3 grid.
+                    The first player to get 3 of their marks in a row diagonally, horizontally, or vertically wins.
+                    If the entire grid is filled, the game is a draw.
+                    """
         case .connect4:
             return """
-Connect Four is a 2 player game where each player takes turn dropping colored discs into a 6x7 suspended grid.
-The first player to get 5 of their discs in a row diagonally, horizontally, or vertically wins.
-If the entire grid is filled, the game is a draw.
-"""
+                    Connect Four is a 2 player game where each player takes turn dropping colored discs into a 6x7 suspended grid.
+                    The first player to get 5 of their discs in a row diagonally, horizontally, or vertically wins.
+                    If the entire grid is filled, the game is a draw.
+                    """
         case .chess:
             return """
-Chess is a 2 player strategy game played on a 8x8 grid. The players take turns moving their pieces (either
-white or black). Each of the different types of pieces have their own rules for moving and can capture
-their opponents pieces.
-In order to win, one player must checkmate the other by threatening to capture their king
-and preventing the king from being able to escape.
-"""
+                    Chess is a 2 player strategy game played on a 8x8 grid. The players take turns moving their pieces (either
+                    white or black). Each of the different types of pieces have their own rules for moving and can capture
+                    their opponents pieces.
+                    In order to win, one player must checkmate the other by threatening to capture their king
+                    and preventing the king from being able to escape.
+                    """
         }
     }
 }
 
-struct Game {
-    let type: AvailableGames
-    let player1: String = "Player 1"
-    let player2: String = "Player 2"
+enum Player {
+    case player0
+    case player1
+}
+
+class Piece {
+    let player: Player
     
+    init(player: Player) {
+        self.player = player
+    }
+}
+
+enum ChessPieceType {
+    case pawn
+    case knight
+    case bishop
+    case rook
+    case queen
+    case king
+}
+
+class ChessPiece: Piece {
+    let type: ChessPieceType
     
+    init(player: Player, type: ChessPieceType) {
+        self.type = type
+        super.init(player: player)
+    }
+}
+
+protocol Game {
+    var gameType: AvailableGames { get }
+    var player0Name: String { get }
+    var player1Name: String { get }
+    var board: [[Piece]] { get set }
+    var whoseTurn: Player { get set }
 }
