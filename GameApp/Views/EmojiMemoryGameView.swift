69d3d5a5a6ff5a041dfc606a609b019b448b833e
Mitchman215 <mitchell_salomon@brown.edu>
2022-01-24 01:54:08 -0500

    Made the size of emojis on cards adaptable. Created CardView.DrawingConstants

--- GameApp/Views/EmojiMemoryGameView.swift
+++ GameApp/Views/EmojiMemoryGameView.swift
@@ -60,19 +60,30 @@ struct CardView: View {
     }
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            } else if card.isMatched{
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader { geo in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content).font(font(in: geo.size))
+                } else if card.isMatched{
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
         }
-        
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
